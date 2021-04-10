a9e690df9bbb6c7db43cdafd4c2b56d71afbc3c9
Feeagh Doo <fiach@apothes.is>
2021-04-10 02:23:26 -0400

    Add property for parent path in output.

--- bcdn-list-files.php
+++ bcdn-list-files.php
@@ -15,9 +15,9 @@ $externalPath = "https://" . $pullZone . ".b-cdn.net";
 $storagePath = $config["storageServer"] . $storageZone;
 $dirs = $config["dirs"];
 
-function getBcdnDirectory($path) {
+function getBcdnDirectory($mainPath, $subPath) {
     global $pass, $storagePath, $externalPath;
-    $curl = curl_init($storagePath . $path);
+    $curl = curl_init($storagePath . $mainPath . $subPath);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($curl, CURLOPT_HTTPHEADER, array("Accept: application/json", "AccessKey: " . $pass));
     $curlResult = curl_exec($curl);
@@ -33,11 +33,15 @@ function getBcdnDirectory($path) {
         curl_close($curl);
         for ($i = 0; $i < count($fileArray); $i++) {
             $file = $fileArray[$i];
-            $fileObj = array("filename" => $file->ObjectName, "directory" => $file->IsDirectory);
-            if ($fileObj["directory"] === true) {
-                $output = array_merge($output, json_decode(getBcdnDirectory($path . $fileObj["filename"] . "/")));
+            $filePath = substr($subPath, 0, -1);
+            if ($filePath === "") {
+                $filePath = null;
             }
-            $fileObj["filename"] = $externalPath . $path . $fileObj["filename"];
+            $fileObj = array("filename" => $file->ObjectName, "path" => $filePath, "directory" => $file->IsDirectory);
+            if ($fileObj["directory"] === true) {
+                $output = array_merge($output, json_decode(getBcdnDirectory($mainPath, $subPath . $fileObj["filename"] . "/")));
+            }
+            $fileObj["filename"] = $externalPath . $mainPath . $subPath . $fileObj["filename"];
             array_push($output, $fileObj);
         }
         return json_encode($output);
@@ -45,7 +49,7 @@ function getBcdnDirectory($path) {
 }
 
 if (array_key_exists("type", $_GET) && array_key_exists($_GET["type"], $dirs)) {
-    print_r(getBcdnDirectory($dirs[$_GET["type"]]));
+    print_r(getBcdnDirectory($dirs[$_GET["type"]], ""));
 } else {
     http_response_code(500);
     print_r(json_encode(array('status' =>'error' ,'code'=> 'input_error')));
